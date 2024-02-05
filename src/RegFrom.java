eb7dc9a9dbc9ff9955c2674f498479ad23431ddc
sarmad12311 <sarmadali0161@gmail.com>
2024-02-05 23:22:06 +0000

    created the layout of the registration form

---
+++ src/RegFrom.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class RegFrom {
+    private JTextField tfName;
+    private JTextField tfEmail;
+    private JTextField tfPhone;
+    private JTextField tfAddress;
+    private JPasswordField pfPassword;
+    private JPasswordField pfConfirmPassword;
+    private JButton btnRegister;
+    private JButton btnCancel;
+    private JPanel regPanel;
+}

8c115accf4dca1621b2559d6fc6a3d30195ad905
sarmad12311 <sarmadali0161@gmail.com>
2024-02-05 23:30:54 +0000

    created a constructor to test it

--- src/RegFrom.java
+++ src/RegFrom.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class RegFrom {
+public class RegFrom extends JDialog{
     private JTextField tfName;
     private JTextField tfEmail;
     private JTextField tfPhone;
@@ -10,4 +11,18 @@ public class RegFrom {
     private JButton btnRegister;
     private JButton btnCancel;
     private JPanel regPanel;
+
+    public RegFrom(JFrame owner) {
+        super(owner);
+        setTitle("Create a new account");
+        setContentPane(regPanel);
+        setMinimumSize(new Dimension(450,474));
+        setModal(true);
+        setLocationRelativeTo(owner);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        RegFrom myForm = new RegFrom(null);
+    }
 }
